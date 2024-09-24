b9120034a9f75a3086a3fbeb4a462eac1e1450b8
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2024-09-24 11:19:35 +0600

    react native navigation setup and configure

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme } from 'react-native';
 import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -17,25 +18,22 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+    <NavigationContainer>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+          <Header />
           <Text className="text-justify m-2 text-[14px] text-black font-font-poppins-medium">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus amet ratione
             voluptatum, in expedita distinctio. Ad est, atque quam cupiditate earum a, molestiae
             consectetur cum ipsam eligendi quibusdam neque repudiandae?
           </Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        </ScrollView>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
