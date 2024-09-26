edac251e18ff615d04dadb22a647be5f51b739bf
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2024-09-26 10:26:16 +0600

    highlight section layout design

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,7 +3,7 @@ import HomeCategory from '@components/utilities/HomeCategory';
 import { useNavigation } from '@react-navigation/native';
 import { COLORS } from '@utils/constant';
 import * as React from 'react';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function HomeScreen(): React.JSX.Element {
@@ -21,44 +21,61 @@ function HomeScreen(): React.JSX.Element {
         <Ionicons name="notifications-outline" size={30} color={COLORS.BLACK} />
       </View>
 
-      {/* category section */}
-      <View className="mt-5 w-11/12 flex flex-row items-center justify-around">
-        <HomeCategory title="Woman" icon="woman" active />
-        <HomeCategory title="Men" icon="man" />
-        <HomeCategory title="Accessories" icon="recording-sharp" />
-        <HomeCategory title="Beauty" icon="sparkles" />
-      </View>
-
-      {/* featured section */}
-      <View className="w-11/12 mt-5">
-        <View className="flex flex-row items-center justify-between my-3">
-          <Text className="text-[26px] font-font-poppins-medium text-black">Feature Product</Text>
-
-          <TouchableOpacity>
-            <Text className="text-[20px] font-font-poppins-regular">Show all</Text>
-          </TouchableOpacity>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* category section */}
+        <View className="mt-5 w-11/12 flex flex-row items-center justify-around">
+          <HomeCategory title="Woman" icon="woman" active />
+          <HomeCategory title="Men" icon="man" />
+          <HomeCategory title="Accessories" icon="recording-sharp" />
+          <HomeCategory title="Beauty" icon="sparkles" />
         </View>
 
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <View className="flex flex-row items-center justify-between" style={{ width: '100%' }}>
-            <FeatureProductCard
-              image={require('@assets/images/feature-1.png')}
-              title="Knight Sweater"
-              price="$ 60.00"
-            />
-            <FeatureProductCard
-              image={require('@assets/images/feature-2.png')}
-              title="Long Sleeve Dress"
-              price="$ 45.00"
-            />
-            <FeatureProductCard
-              image={require('@assets/images/feature-3.png')}
-              title="Sportwear Set"
-              price="$ 80.00"
-            />
+        {/* featured section */}
+        <View className="w-11/12 mt-5 mx-auto">
+          <View className="flex flex-row items-center justify-between my-3">
+            <Text className="text-[26px] font-font-poppins-medium text-black">Feature Product</Text>
+
+            <TouchableOpacity>
+              <Text className="text-[20px] font-font-poppins-regular">Show all</Text>
+            </TouchableOpacity>
           </View>
-        </ScrollView>
-      </View>
+
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View className="flex flex-row items-center justify-between" style={{ width: '100%' }}>
+              <FeatureProductCard
+                image={require('@assets/images/feature-1.png')}
+                title="Knight Sweater"
+                price="$ 60.00"
+              />
+              <FeatureProductCard
+                image={require('@assets/images/feature-2.png')}
+                title="Long Sleeve Dress"
+                price="$ 45.00"
+              />
+              <FeatureProductCard
+                image={require('@assets/images/feature-3.png')}
+                title="Sportwear Set"
+                price="$ 80.00"
+              />
+            </View>
+          </ScrollView>
+        </View>
+
+        {/* highlights section */}
+        <View className="bg-[#F8F8FA] my-2 px-4 w-full flex flex-row items-center justify-between">
+          <View className="flex-1">
+            <Text className="uppercase text-[17px] font-font-poppins-light">I new collection</Text>
+            <Text className="uppercase text-[25px] font-font-poppins-regular">
+              Hang out & Party
+            </Text>
+          </View>
+
+          <Image
+            className="flex-1 object-contain"
+            source={require('@assets/images/group-frame.png')}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
