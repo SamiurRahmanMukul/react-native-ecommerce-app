ff319f9eddc761e5571a1797e8c9bad73c980e7b
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2024-09-26 17:05:57 +0600

    search bar section layout design

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import { COLORS } from '@utils/constant';
 import * as React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function SearchScreen(): React.JSX.Element {
@@ -18,8 +18,33 @@ function SearchScreen(): React.JSX.Element {
         <Text className="text-[22px] text-black font-font-poppins-medium">Discover</Text>
         <Ionicons name="notifications-outline" size={30} color={COLORS.BLACK} />
       </View>
+
+      <ScrollView className="mb-14 w-11/12" showsVerticalScrollIndicator={false}>
+        <View className="flex flex-row items-center justify-between">
+          <TextInput
+            className="bg-white w-[82%] h-[50px] rounded-md p-3 font-font-poppins-light text-black"
+            style={styles.shadowProp}
+            placeholder="Search"
+          />
+
+          <View
+            className="w-[50px] h-[50px] bg-white rounded-md justify-center items-center"
+            style={styles.shadowProp}>
+            <Ionicons className="p-3" name="options" size={25} color={COLORS.BLACK} />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+});
 
 export default SearchScreen;
